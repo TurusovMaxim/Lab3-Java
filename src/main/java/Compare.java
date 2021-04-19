07b6d36f0a5f0cbc2b9378ab5ebf3cf267f5cb4c
Maxim Turusov <turusovmaxim@gmail.com>
2021-04-20 01:30:48 +0300

    проделана основная работа для данного задания

---
+++ src/main/java/Compare.java
@@ -0,0 +1,159 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Класс для подсчета времени выполнения основных методов реализаций интерфейса List
+ */
+public class Compare {
+
+    private long begin;
+    private long end;
+    private String method;
+    private String listType;
+
+
+    public Compare(long begin, long end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     *
+     * метод используется для определения точной реализации List,
+     * используя оператор instanceOf
+     */
+
+    private void checkListType(List<Integer> list){
+        if (list instanceof ArrayList) {
+            listType = "Arraylist";
+        } else {
+            listType = "LinkedList";
+        }
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     * @param reiterations - количество повторений выполнения метода
+     *
+     * метод используется для измерения времени выполнения добавления
+     * элементов в конец конкретной реализации List
+     */
+    public void compareAddToEnd(List<Integer> list, long reiterations) {
+        method = "compareAddToEnd";
+
+        checkListType(list);
+
+        begin = System.currentTimeMillis();
+
+        for (int i = 0; i < reiterations; i++) {
+            list.add(i);
+        }
+        end = System.currentTimeMillis();
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     * @param reiterations - количество повторений выполнения метода
+     *
+     * метод используется для измерения времени выполнения получения,
+     * используя конкретную реализацию List
+     */
+    public void compareGet(List<Integer> list, long reiterations) {
+        method = "compareGet";
+
+        checkListType(list);
+
+        begin = System.currentTimeMillis();
+
+        for (int i = 0; i < reiterations; i++) {
+            list.get(i);
+        }
+
+        end = System.currentTimeMillis();
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     *
+     * метод используется для измерения времени выполнения удаления,
+     * используя конкретную реализацию List (with Iterator)
+     */
+    public void compareRemove(List<Integer> list) {
+        method = "compareRemove";
+
+        checkListType(list);
+
+        begin = System.currentTimeMillis();
+
+        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
+            if (list.contains(iterator.next())) {
+                iterator.remove();
+            }
+        }//O(n^2) method clear() is better - O(n)
+        end = System.currentTimeMillis();
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     *
+     * метод используется для измерения времени выполнения удаления,
+     * используя конкретную реализацию List (with Indexes)
+     */
+    public void compareRemoveWithIndex(List<Integer> list) {
+        method = "compareRemoveWithIndex";
+
+        checkListType(list);
+
+        begin = System.currentTimeMillis();
+
+        for (int i = 0; i < list.size(); i++) {
+            list.remove(i);
+        }
+
+        end = System.currentTimeMillis();
+    }
+
+
+    /**
+     * @param list - реализация интерфейса List
+     *
+     * метод используется при измерении времени выполнения
+     * проверки каждого элемента на наличие в
+     * конкретной реализации List
+     */
+    public void compareForEach(List<Integer> list) {
+        method = "compareForEach";
+
+        checkListType(list);
+
+        begin = System.currentTimeMillis();
+
+        list.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                list.contains(integer);
+            }
+        });//can be replaced with lambda exp
+
+        end = System.currentTimeMillis();
+    }
+
+
+    /**
+     * @return - результат измерений
+     */
+    @Override
+    public String toString() {
+        return (end - begin) + " ms " +
+                " " + method + " " +
+                listType + "\n";
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long begin = 0;
+        long end = 0;
+        long reiterations = 100000;
+
+        String result;
+
+        Compare compare = new Compare(begin, end);
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        String title = "\nTime (ms)  " + "  method  " + "  list type\n";
+        System.out.println(title);
+
+        //return result
+        result = compare.toString();
+
+        //add
+        compare.compareAddToEnd(arrayList, reiterations);
+        System.out.println(compare.toString());
+
+        compare.compareAddToEnd(linkedList, reiterations);
+        System.out.println(compare.toString());
+
+        //get
+        compare.compareGet(arrayList, reiterations);
+        System.out.println(compare.toString());
+
+        compare.compareGet(linkedList, reiterations);
+        System.out.println(compare.toString());
+
+        //remove with Iterator
+        compare.compareRemove(arrayList);
+        System.out.println(compare.toString());
+
+        compare.compareRemove(linkedList);
+        System.out.println(compare.toString());
+
+        //add again..
+        compare.compareAddToEnd(arrayList, reiterations);
+        compare.compareAddToEnd(linkedList,reiterations);
+
+        //remove with indexes
+        compare.compareRemoveWithIndex(arrayList);
+        System.out.println(compare.toString());
+
+        compare.compareRemoveWithIndex(linkedList);
+        System.out.println(compare.toString());
+
+        //foreach
+        compare.compareForEach(arrayList);
+        System.out.println(compare.toString());
+
+        compare.compareForEach(linkedList);
+        System.out.println(compare.toString());
+    }
+}
